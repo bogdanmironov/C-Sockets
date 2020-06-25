1257a04a1a2d4d005228b250eef21a18bffdd8ef
bogdanmironov <bogdanmironow@gmail.com>
2020-06-25 15:29:49 +0300

    made the menu update and kept faiures as comments

--- Client.c
+++ Client.c
@@ -21,7 +21,7 @@ int number_users = 0;
 
 void *print_recieved_messages(void* socket); //Thread function
 void *menu(void *socket); //Thread function
-void *update_users(void *socket); //Thread function
+void update_users(int socket); //Thread function
 void init_client(int sock);
 
 pthread_mutex_t m_print_message; //Mutex start with m_
@@ -33,9 +33,8 @@ void create_menu(int sock);
 void create_username_updater(int sock);
 void choose_username(int sock);
 void read_and_send_messages(int sock, char *recipent);
+int receive_int(int *num, int fd);
 
-
-// WINDOW *win;
 
 int main(int argc, char *argv[]) {
     mutex_init();
@@ -62,6 +61,7 @@ void mutex_init() {
 
 void read_and_send_message(int sock, char *recipient) {
     char message[MAX_MESSAGE_SIZE];
+    int size_message = 0;
 
     getstr(message);
     strtok(message, "\n");
@@ -91,22 +91,10 @@ void choose_username(int sock) {
 }
 
 void init_client(int sock) {
-    create_username_updater(sock);
-    sleep(1);
-    create_menu(sock);
     create_printer(sock);
+    create_menu(sock);
 }
 
-void create_username_updater(int sock) {
-    pthread_t thread;
-
-    int rc = pthread_create(&thread, NULL, update_users, (void *) (intptr_t)sock);
-
-    if (rc) {
-        printf("ERROR; pthread_create(update_users) return %d\n", rc);
-        exit(-1);
-    }
-}
 
 void create_menu(int sock) {
     pthread_t thread;
@@ -155,43 +143,6 @@ int connect_to_server() {
     return sock;
 }
 
-void *update_users(void *socket) {
-    int sock = (intptr_t) socket;
-    fd_set readfds; 
-    int max_sd = sock;
-
-    for ever {
-        FD_ZERO(&readfds);
-
-        FD_SET(sock, &readfds);
-        max_sd = sock;
-
-        int activity = select( max_sd + 1 , &readfds , NULL , NULL , NULL);  
-
-        if ((activity < 0) && (errno!=EINTR)) {   
-            printf("select error");   
-        }   
-
-        if ( FD_ISSET(sock, &readfds) ) {
-            // if (recv(sock, &number_users, sizeof(number_users), MSG_PEEK) == 0) continue;
-            pthread_mutex_lock(&m_print_message);
-
-            recv(sock, &number_users, sizeof(number_users), 0);
-
-            number_users = ntohl(number_users);
-            printf("%d\n", number_users);
-
-            for (int i = 0; i < number_users; ++i) {
-                int len_username;
-                recv(sock, &len_username, sizeof(unsigned int), 0);
-                recv(sock, usernames[i], len_username, 0);
-            }
-            pthread_mutex_unlock(&m_print_message); //we use mutex to ensure that we are not printing obsolete info
-            // sleep(1);
-        }
-    }
-}
-
 void *menu(void *socket) {
     initscr(); // Curses init
     int sock = (intptr_t) socket;
@@ -199,20 +150,22 @@ void *menu(void *socket) {
     int position = 0;
     int input;
 
-    
-
     for ever {
-        // printf("HI\n");
-        // update_users(sock);
-        pthread_mutex_lock(&m_print_message);
+        // pthread_mutex_lock(&m_print_message);
         int pos = 0;
         move(pos,0);
         cbreak(); //Get a character at a time
         noecho();
         keypad(stdscr, TRUE);
         
+        clear();
         addstr("Active users:\n");
+        // char snum[5];
+        // itoa(number_users, snum, 10);
+        // addstr(snum);
+        // printw("%d", number_users);
         for ( int print = 0; print < number_users; print++ ) {
+            // addstr("hi");
             pos++;
 
             move(pos, 0);
@@ -223,36 +176,29 @@ void *menu(void *socket) {
         }
 
         move(++pos, 0);
-        addstr("Press arrow keys to move,Enter to choose;\n");
+        addstr("Press arrow keys to move,Enter to choose;");
 
         refresh();
 
-        // pthread_mutex_unlock(&m_print_message);
 
 
         input = getch();
-        // system("clear");
-
-        // if( input == 224 || input == 0 ){
-        //     input = getch();
-        // }
 
         switch( input ){
             case KEY_UP: if( position > 0 ) position -= 1; break;
             case KEY_DOWN: if( position < number_users - 1 ) position+=1; break;
-        }
-
-        
-        // pthread_mutex_lock(&m_print_message);
-
-        if( input == KEY_ENTER ){
-            // system("clear");
-            addstr("Enter message:\n");
-            read_and_send_message(sock, usernames[position]);
+            case '\n': 
+                move(++pos, 0);
+                addstr("Enter message:");
+                read_and_send_message(sock, usernames[position]);
+                break;
+            default:
+                break;
         }
 
         nocbreak(); //Restore default settings
-        pthread_mutex_unlock(&m_print_message);
+        echo();
+        // pthread_mutex_unlock(&m_print_message);
     }
 }
 
@@ -264,41 +210,100 @@ void *print_recieved_messages(void *socket) {
     char *token;
 
     for ever {
-        char buffer[1024] = {0};
-        if (recv(sock, buffer, 1024, 0) == 0) {
-            pthread_exit(0);
+        char type_of_msg;
+        recv(sock, &type_of_msg, 1, 0);
+
+        if (type_of_msg == '@') { //Message
+            // addstr("@");
+            char buffer[1024] = {0};
+            if (recv(sock, buffer, 1024, 0) == 0) {
+                pthread_exit(0);
+            }
+
+            // pthread_mutex_lock(&m_print_message);
+
+            if (strcmp("E404", buffer) == 0) {
+                addstr("There is no such username.\n");
+                continue;
+            }
+
+            if (strcmp("E400", buffer) == 0) {
+                addstr("Invalid form8at.[Recipient#Message]\n");
+                continue;
+            }
+
+            int h, w;
+            getmaxyx(stdscr, h, w);
+            move( h-4, 0 );
+            token = strtok(buffer, delim);
+            addstr("Recieved a message from ");
+            addstr(token);
+            addstr("\n");
+            token = strtok(NULL, delim);
+            addstr("Message: ");
+            addstr(token);
+            addstr("\n");
+        } else if (type_of_msg == '$') { //Header
+            // addstr("$");
+            // pthread_mutex_lock(&m_print_message);
+            // int number_users_n;
+            // recv(sock, &number_users_n, sizeof(number_users_n), 0);
+            // receive_int(&number_users, sock);
+            // number_users = ntohl(number_users);
+            // addstr(number_users);
+            // char snum[5];
+            // recv(sock, snum, 5, 0);
+            // number_users = atoi(snum);
+            // addstr(number_users);
+            char number[1];
+            recv(sock, &number, 1, 0);
+            number_users = atoi(number);
+
+            addstr(number);
+
+            for (int i = 0; i < number_users; ++i) {
+                int len_username;
+                recv(sock, &len_username, sizeof(unsigned int), 0);
+                recv(sock, usernames[i], len_username, 0);
+            }
+            // pthread_mutex_unlock(&m_print_message);
+
         }
 
-        pthread_mutex_lock(&m_print_message);
 
-        if (strcmp("E404", buffer) == 0) {
-            addstr("There is no such username.\n");
-            continue;
-        }
-
-        if (strcmp("E400", buffer) == 0) {
-            addstr("Invalid format.[Recipient#Message]\n");
-            continue;
-        }
-
-        int h, w;
-        getmaxyx(stdscr, h, w);
-        move( h-4, 0 );
-        token = strtok(buffer, delim);
-        addstr("Recieved a message from ");
-        addstr(token);
-        addstr("\n");
-        token = strtok(NULL, delim);
-        addstr("Message: ");
-        addstr(token);
-        addstr("\n");
+        
         // sleep(1);
 
         // printf("\nPress key to continue\n");
         // getchar();
         // printf("");
 
-        pthread_mutex_unlock(&m_print_message);
+        // pthread_mutex_unlock(&m_print_message);
     }
 
 }
+
+// int receive_int(int *num, int fd) {
+//     int32_t ret;
+//     char *data = (char*)&ret;
+//     int left = sizeof(ret);
+//     int rc;
+//     do {
+//         rc = read(fd, data, left);
+//         if (rc <= 0) { /* instead of ret */
+//             if ((errno == EAGAIN) || (errno == EWOULDBLOCK)) {
+//                 // use select() or epoll() to wait for the socket to be readable again
+//             }
+//             else if (errno != EINTR) {
+//                 return -1;
+//             }
+//         }
+//         else {
+//             data += rc;
+//             left -= rc;
+//         }
+//     }
+//     while (left > 0);
+//     *num = ntohl(ret);
+//     return 0;
+// }
--- Server.c
+++ Server.c
@@ -15,6 +15,7 @@
 
 char usernames[MAX_CLIENTS][MAX_USERNAME_LEN];
 int number_of_clients = 0;
+int server_socket;
 
 void get_message_and_leave_reciever(char*, char*);
 int contains(char usernames[MAX_CLIENTS][MAX_USERNAME_LEN], char *buffer);
@@ -23,12 +24,17 @@ int setup_select(int server_socket, fd_set *readfds, int *client_sockets);
 void add_socket(int server_socket, struct sockaddr_in address, int (*client_sockets)[MAX_CLIENTS]);
 void add_socket_to_list(int new_socket, int (*client_sockets)[MAX_CLIENTS], char *name);
 void relay_message(int *client_sockets, fd_set readfds);
+void send_usernames(int sock, int *client_sockets);
+void send_message(char buffer[1024], int *client_sockets, int username_num);
+void send_usernames_to_everyone(int *client_sockets);
+int send_int(int num, int fd);
+char* itoa(int num, char* str, int base);
 
 int main (int argc, char *argv[]) {
 
     int client_sockets[MAX_CLIENTS] = {0};
     struct sockaddr_in address;
-    int server_socket = setup_server(&address);
+    server_socket = setup_server(&address);
     fd_set readfds;
 
     for ever {
@@ -64,36 +70,78 @@ void relay_message(int *client_sockets, fd_set readfds) {
                 printf("%s disconected.\n", usernames[i]);
 
                 close( sd );   
-                client_sockets[i] = 0; 
+                client_sockets[i] = 0;
+                strcpy(usernames[i], "");
+
+                send_usernames_to_everyone(client_sockets);
+
             } else {
-                if( strstr(buffer, "#") == NULL ) {
-                    send(client_sockets[i], "E400", 4, 0);
-                    break;
-                }
-
                 buffer[valread] = '\0';  
-                
-                char message[MAX_MESSAGE_SIZE];
-                get_message_and_leave_reciever(buffer, message);
-
-                int recipient_id = contains(usernames, buffer);
-
-                if ( recipient_id == -1 ) {
-                    printf("Such username was not found.\n");
-                    send(client_sockets[i], "E404", 4, 0);
-                    break;
-                }
-
-                memset(buffer, 0, sizeof(buffer));
-                strcat(buffer, usernames[i]);
-                strcat(buffer, "#");
-                strcat(buffer, message);
-
-                send(client_sockets[recipient_id], buffer, strlen(buffer), 0);
-
+                send_message(buffer, client_sockets, i);
             }
         }
     }
+}
+
+void send_usernames(int socket, int *client_sockets) {
+    send(socket, "$", 1, 0); //Sends type of msg
+    // int number_of_clients_n = htonl(number_of_clients);
+    // printf("%d\n", number_of_clients);
+    // send(socket, &number_of_clients_n, sizeof(number_of_clients_n), 0);
+    printf("%d\n", number_of_clients);
+    char number;
+    number = number_of_clients + 48;
+    printf("%c\n", number);
+    send(socket, &number, 1, 0);
+    // send_int(number_of_clients, socket);
+    // char snum[5];
+    // itoa(number_of_clients, snum, 10);
+    // send(socket, snum, 5, 0);
+
+    for (int i = 0; i < number_of_clients; ) {
+        if (client_sockets[i] != 0) {
+            unsigned int len_username = strlen(usernames[i]);
+            send(socket, &len_username, sizeof(unsigned int), 0);
+            send(socket, usernames[i], len_username, 0);
+            printf("%s\n", usernames[i]);
+            ++i;
+        }
+    }//Send usernames
+}
+
+void send_usernames_to_everyone(int *client_sockets) {
+    for (int i = 0; i < MAX_CLIENTS; ++i) {
+        if (client_sockets[i] != 0) {
+            if (client_sockets[i] == server_socket) continue; 
+            send_usernames(client_sockets[i], client_sockets);
+        }
+    }
+}
+
+void send_message(char buffer[1024], int *client_sockets, int username_num) {
+    if( strstr(buffer, "#") == NULL ) {
+        send(client_sockets[username_num], "E400", 4, 0);
+        return;
+    }
+
+    char message[MAX_MESSAGE_SIZE];
+    get_message_and_leave_reciever(buffer, message);
+
+    int recipient_id = contains(usernames, buffer);
+
+    if ( recipient_id == -1 ) {
+        printf("Such username was not found.\n");
+        send(client_sockets[username_num], "E404", 4, 0);
+        return;
+    }
+
+    memset(buffer, 0, sizeof(char) * 1024);
+    strcat(buffer, usernames[username_num]);
+    strcat(buffer, "#");
+    strcat(buffer, message);
+
+    send(client_sockets[recipient_id], "@", sizeof("@"), 0); //Sends type of msg
+    send(client_sockets[recipient_id], buffer, strlen(buffer), 0);
 }
 
 void add_socket(int server_socket, struct sockaddr_in address, int (*client_sockets)[MAX_CLIENTS]){
@@ -113,20 +161,21 @@ void add_socket(int server_socket, struct sockaddr_in address, int (*client_sock
 
     add_socket_to_list(new_socket, &*client_sockets, buffer);
 
-    for (int curr_sock = 0; curr_sock < number_of_clients; ++curr_sock) {
-        int number_of_clients_n = htonl(number_of_clients);
-        // printf("%d\n", number_of_clients);
-        send((*client_sockets)[curr_sock], &number_of_clients_n, sizeof(number_of_clients), 0);
+    sleep(1);
 
-        for (int i = 0; i < number_of_clients; ++i) {
-            unsigned int len_username = strlen(usernames[i]);
-            send((*client_sockets)[curr_sock], &len_username, sizeof(unsigned int), 0);
-            send((*client_sockets)[curr_sock], usernames[i], len_username, 0);
-            // printf("%s\n", usernames[i]);
-        }
-    }
+    send_usernames_to_everyone(*client_sockets);
+    printf("Sent to newbie\n");
+    // int number_of_clients_n = htonl(number_of_clients);
+    // printf("%d\n", number_of_clients);
+    // send(new_socket, &number_of_clients_n, sizeof(number_of_clients), 0);
 
-    
+    // for (int i = 0; i < number_of_clients; ++i) {
+    //     unsigned int len_username = strlen(usernames[i]);
+    //     send(new_socket, &len_username, sizeof(unsigned int), 0);
+    //     send(new_socket, usernames[i], len_username, 0);
+    //     printf("%s\n", usernames[i]);
+    // }
+
 
 }
 
@@ -206,6 +255,87 @@ void get_message_and_leave_reciever(char *buffer, char *msg) {
 
     strcpy(msg, token);
 }
+
+// void swap(char *str1, char *str2) { 
+//   char *temp = str1; 
+//   str1 = str2; 
+//   str2 = temp; 
+// }   
+
+// void reverse(char str[], int length) { 
+//     int start = 0; 
+//     int end = length -1; 
+//     while (start < end) 
+//     { 
+//         swap((str+start), (str+end)); 
+//         start++; 
+//         end--; 
+//     } 
+// } 
+
+// char* itoa(int num, char* str, int base) { 
+//     int i = 0; 
+//     int isNegative = 0; 
+//   
+//    /* Handle 0 explicitely, otherwise empty string is printed for 0 */
+    // if (num == 0) 
+    // { 
+    //     str[i++] = '0'; 
+    //     str[i] = '\0'; 
+    //     return str; 
+    // } 
+//   
+    // // In standard itoa(), negative numbers are handled only with  
+    // // base 10. Otherwise numbers are considered unsigned. 
+//     if (num < 0 && base == 10) 
+//     { 
+//         isNegative = 1; 
+//         num = -num; 
+//     } 
+//   
+//     // Process individual digits 
+//     while (num != 0) 
+//     { 
+//         int rem = num % base; 
+//         str[i++] = (rem > 9)? (rem-10) + 'a' : rem + '0'; 
+//         num = num/base; 
+//     } 
+//   
+//     // If number is negative, append '-' 
+//     if (isNegative) 
+//         str[i++] = '-'; 
+//   
+//     str[i] = '\0'; // Append string terminator 
+//   
+//     // Reverse the string 
+//     reverse(str, i); 
+//   
+//     return str; 
+// } 
+
+// int send_int(int num, int fd) {
+//     int32_t conv = htonl(num);
+//     char *data = (char*)&conv;
+//     int left = sizeof(conv);
+//     int rc;
+//     do {
+//         rc = write(fd, data, left);
+//         if (rc < 0) {
+//             if ((errno == EAGAIN) || (errno == EWOULDBLOCK)) {
+//                 // use select() or epoll() to wait for the socket to be writable again
+//             }
+//             else if (errno != EINTR) {
+//                 return -1;
+//             }
+//         }
+//         else {
+//             data += rc;
+//             left -= rc;
+//         }
+//     }
+//     while (left > 0);
+//     return 0;
+// }
 
 int contains(char usernames[MAX_CLIENTS][MAX_USERNAME_LEN], char *buffer) {
     int contains = -1;
